a798c80ff16f3da4837036af56f12e24bb3edb8d
Himsa Yudhistira <himsas53@gmail.com>
2018-10-18 13:47:08 +0700

    fourth

--- app/src/main/java/com/example/layanacomputindo/test/CurrentLocationActivity.kt
+++ app/src/main/java/com/example/layanacomputindo/test/CurrentLocationActivity.kt
@@ -8,9 +8,13 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import im.delight.android.location.SimpleLocation
+
+
 
 class CurrentLocationActivity : AppCompatActivity(), OnMapReadyCallback {
-    val mLOCATION = LatLng(-7.792662, 110.366073)
+    private lateinit var location: SimpleLocation
+    private lateinit var mLOCATION: LatLng
     val ZOOM_LEVEL = 18f
     override fun onMapReady(googleMap: GoogleMap?) {
         googleMap ?: return
@@ -26,5 +30,36 @@ class CurrentLocationActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment : SupportMapFragment? =
                 supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
+        // construct a new instance of SimpleLocation
+        location = SimpleLocation(this)
+
+        // if we can't access the location yet
+        if (!location.hasLocationEnabled()) {
+            // ask the user to enable location access
+            SimpleLocation.openSettings(this)
+        }
+        mLOCATION = LatLng(location.latitude, location.longitude)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        // make the device update its location
+        if (true) {
+            location.beginUpdates()
+        }
+
+        // ...
+    }
+
+    override fun onPause() {
+        // stop location updates (saves battery)
+        if (true) {
+            location.endUpdates()
+        }
+
+        // ...
+
+        super.onPause()
     }
 }
--- app/src/main/java/com/example/layanacomputindo/test/MainActivity.kt
+++ app/src/main/java/com/example/layanacomputindo/test/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.layanacomputindo.test
 
+import android.Manifest
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import pub.devrel.easypermissions.EasyPermissions
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onClick(v: View?) {
@@ -21,6 +24,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var perms = Manifest.permission.ACCESS_FINE_LOCATION
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            // Have permissions, do the thing!
+        } else {
+            // Ask for permissions
+            EasyPermissions.requestPermissions(this, getString(R.string.rationale_location_contacts),
+                    1, perms)
+        }
 
         button.setOnClickListener(this)
         button2.setOnClickListener(this)
